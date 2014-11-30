b56a236e7df7ba9c3bbd5a8526060138be1b6a1a
redcirclegame <vladislavz1991@gmail.com>
2014-11-29 21:25:13 -0500

    updated menu

--- project.spritebuilder/Source/MainScene.m
+++ project.spritebuilder/Source/MainScene.m
@@ -2,4 +2,13 @@
 
 @implementation MainScene
 
+
+- (void) playTapped
+{
+    CCScene * gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
+    CCTransition * transition = [CCTransition transitionCrossFadeWithDuration:0.2];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene withTransition:transition];
+}
+
+
 @end
